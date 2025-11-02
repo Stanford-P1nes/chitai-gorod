7c0603766320bee407712ec167897725b29ae20d
pines10110@gmail.com <pines10110@gmail.com>
2025-11-02 12:58:13 +0300

    refactor(ui-button) Сhanged the jsvascript code to typescript

--- components/Ui/Button.vue
+++ components/Ui/Button.vue
@@ -1,43 +1,43 @@
-<script setup>
-// interface Props {
-//     variant?: 'primary' | 'secondary';
-//     text?: string;
-//     src?: string;
-//     alt?: string;
-//     disable?: boolean;
-// }
+<script setup lang='ts'>
+interface Props {
+    variant?: 'primary' | 'secondary';
+    text?: string;
+    src?: string;
+    alt?: string;
+    disable?: boolean;
+}
 
-// const props = withDefaults(defineProps<Props>(), {
-//     variant: 'primary',
-//     text: '',
-//     src: '@/assets/images/ico/image-not-found-01.svg',
-//     alt: 'Изображение',
-//     disable: false,
-// });
-
-const props = defineProps({
-    variant: {
-        type: String,
-        default: 'primary',
-        validator: value => ['primary', 'secondary'].includes(value),
-    },
-    text: {
-        type: String,
-        default: '',
-    },
-    src: {
-        type: String,
-        default: '@/assets/images/ico/image-not-found-01.svg',
-    },
-    alt: {
-        type: String,
-        default: 'Изображение',
-    },
-    disable: {
-        type: Boolean,
-        default: false,
-    },
+const props = withDefaults(defineProps<Props>(), {
+    variant: 'primary',
+    text: '',
+    src: '@/assets/images/ico/image-not-found-01.svg',
+    alt: 'Изображение',
+    disable: false,
 });
+
+// const props = defineProps({
+//     variant: {
+//         type: String,
+//         default: 'primary',
+//         validator: value => ['primary', 'secondary'].includes(value),
+//     },
+//     text: {
+//         type: String,
+//         default: '',
+//     },
+//     src: {
+//         type: String,
+//         default: '@/assets/images/ico/image-not-found-01.svg',
+//     },
+//     alt: {
+//         type: String,
+//         default: 'Изображение',
+//     },
+//     disable: {
+//         type: Boolean,
+//         default: false,
+//     },
+// });
 </script>
 
 <template>
