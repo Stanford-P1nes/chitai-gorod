1f04f7e473e63c6b052f3b2093a39a575970c357
pines10110@gmail.com <pines10110@gmail.com>
2025-11-02 19:20:48 +0300

    refactor(style) Changed the design drop-down of the ui-input component

--- components/Ui/Input.vue
+++ components/Ui/Input.vue
@@ -2,7 +2,6 @@
 import { ref } from 'vue';
 import { Country, countryData } from '~/types/country';
 
-// Теперь используем импортированные данные
 const country = ref<Country>(Country.RUSSIA);
 const showListCountry = ref<boolean>(false);
 const phoneNumber = ref<string>('');
@@ -11,10 +10,14 @@ const toggleCountryList = (): void => {
     showListCountry.value = !showListCountry.value;
 };
 
-// Получаем данные из централизованного объекта
 const getCurrentFlag = (): string => countryData[country.value].flag;
 const getCurrentPlaceholder = (): string => countryData[country.value].placeholder;
 const getCountryName = (countryCode: Country): string => countryData[countryCode].name;
+
+const selectCountry = (selectedCountry: Country): void => {
+    country.value = selectedCountry;
+    showListCountry.value = false;
+};
 </script>
 
 <template>
@@ -33,20 +36,32 @@ const getCountryName = (countryCode: Country): string => countryData[countryCode
                         :alt="`Флаг ${country}`"
                     />
                 </div>
-                <select
-                    name="country"
+                <div
                     class="ui-input__select"
-                    v-show="showListCountry"
-                    v-model="country"
+                    :class="{ 'ui-input__select--active': showListCountry }"
                 >
-                    <option
+                    <button
                         v-for="countryCode in Object.values(Country)"
                         :key="countryCode"
                         :value="countryCode"
+                        class="ui-input__button ui-input__button--hover"
+                        @click="selectCountry(countryCode)"
                     >
-                        {{ getCountryName(countryCode) }}
-                    </option>
-                </select>
+                        <div class="ui-input__button-content">
+                            <img
+                                :src="countryData[countryCode].flag"
+                                :alt="countryData[countryCode].name"
+                                class="ui-input__button-flag"
+                            />
+                            <span class="ui-input__button-name">
+                                {{ countryData[countryCode].name }}
+                            </span>
+                            <span class="ui-input__button-placeholder">
+                                {{ countryData[countryCode].placeholder }}
+                            </span>
+                        </div>
+                    </button>
+                </div>
             </div>
             <input
                 type="tel"
@@ -57,7 +72,6 @@ const getCountryName = (countryCode: Country): string => countryData[countryCode
                 autocomplete="off"
                 autofocus
                 required
-                @focus="toggleCountryList"
             />
         </div>
     </div>
@@ -81,6 +95,7 @@ const getCountryName = (countryCode: Country): string => countryData[countryCode
         width: 20px;
         height: 20px;
         cursor: pointer;
+        user-select: none;
     }
 
     &__img {
@@ -90,14 +105,66 @@ const getCountryName = (countryCode: Country): string => countryData[countryCode
 
     &__select {
         position: absolute;
-        right: 100%;
-        bottom: 0;
+        left: 0;
+        top: 100%;
+        width: 100%;
+        border: $border;
+        border-radius: $border-r-md;
+        background-color: $color-white-a; 
+        padding: 2px;
+        display: flex;
+        flex-direction: column;
+        gap: $gap-xs;
         cursor: pointer;
         font-family: $font-family-text;
         font-weight: 400;
         font-size: $font-size-lg;
         line-height: $line-height-xl;
         color: $color-black;
+        overflow: hidden;
+        max-height: 0;
+        opacity: 0;
+        transform: translateY(-10px);
+        transition: all 0.3s ease;
+
+        &--active {
+            max-height: 1000px;
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+
+    &__button {
+        flex-grow: 1;
+        text-align: start;
+        padding: 12px 17px;
+        border: $border;
+        border-radius: $border-r-sm;
+        background-color: $color-white;
+        transition: background 0.2s linear;
+        &--hover:hover {
+            background-color: $color-orange;
+            color: $color-white;
+        }
+
+        &-content {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        &-flag {
+            width: 20px;
+            height: 20px;
+            user-select: none;
+        }
+
+        &-name, &-placeholder {
+            font-family: $font-family-text;
+            font-weight: 400;
+            font-size: $font-size-md;
+            line-height: $line-height-xl;
+            color: $color-black;
+        }
     }
 
     &__input {
