18bb18085308fc94ab18fd7681db06963babc65c
pines10110@gmail.com <pines10110@gmail.com>
2025-11-06 17:24:35 +0300

    feat(data) Created a type and interface for these products.

---
+++ types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: number;
+    img: string;
+    alt: string;
+    price: number;
+    oldPrice: number;
+    currency: string;
+    discount: string;
+    name: string;
+    author: string;
+    rating: number;
+}
