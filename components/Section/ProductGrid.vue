2664a199ab065b92e70579646bcfcf4d7a4c53fb
pines10110@gmail.com <pines10110@gmail.com>
2025-11-16 19:05:28 +0300

    feat: Added a general section component for cards

---
+++ components/Section/ProductGrid.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="product-grid">
+        <slot />
+    </section>
+</template>
+
+<style scoped lang="scss">
+.product-grid {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    gap: 20px;
+}
+</style>
