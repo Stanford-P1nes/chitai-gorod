3363726b02e70208cc07c4e3bae1cb93eba0e250
pines10110@gmail.com <pines10110@gmail.com>
2025-12-21 12:25:06 +0300

    feat: Add users data, types and store

---
+++ stores/users.ts
@@ -0,0 +1,45 @@
+import type { User } from '~/types/user';
+import { users } from '~/data/users';
+import { ref, nextTick } from 'vue';
+import { defineStore } from 'pinia';
+import { useSupabase } from '~/composables/useSupabase';
+import { transformKeysToCamel } from '~/utils/transformKeys';
+
+export const useUsersStore = defineStore('users', () => {
+    const { supabase } = useSupabase();
+    const usersData = ref<User[]>([]);
+    const loadingUsers = ref(false);
+
+    async function getUsers() {
+        loadingUsers.value = true;
+        nextTick();
+
+        const setFallbackUsers = () => {
+            usersData.value = users;
+        };
+
+        try {
+            const { data, error } = await supabase.from('users').select<User[]>('*');
+
+            if (error) {
+                console.error('Supabase error:', error);
+                setFallbackUsers();
+                return;
+            }
+
+            usersData.value = transformKeysToCamel<User[]>(data);
+        } catch (error) {
+            console.error('Произошла ошибка во время получуния данных: ', error);
+            
+            setFallbackUsers();
+        } finally {
+            loadingUsers.value = false;
+        }
+    }
+
+    return {
+        usersData,
+        loadingUsers,
+        getUsers,
+    };
+});
---
+++ types/user.ts
@@ -0,0 +1,7 @@
+export interface User {
+    id: number;
+    username: string;
+    phoneNumber: string;
+    created_at: string;
+    updated_at: string;
+}
