e6c8aa60423a478d8dfe0bf999016dc14a2463c2
pines10110@gmail.com <pines10110@gmail.com>
2025-12-21 11:20:52 +0300

    refactor: update review data and types

--- types/review.ts
+++ types/review.ts
@@ -2,10 +2,14 @@ import type { Rating } from '~/types/rating';
 
 export interface Review {
     id: number;
+    userId?: number | null;
     productId: number;
-    author: string;
+    authorName?: string | null;
     text: string;
+    prosText?: string;
+    consText?: string;
     rating: Rating;
-    prosText: string;
-    consText: string;
+    verifiedPurchase: boolean;
+    created_at: string;
+    updated_at?: string | null;
 }
